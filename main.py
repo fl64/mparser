7469d19768ae629636a286cf19751adc3f889393
fl64 <flsixtyfour@gmail.com>
2018-01-30 17:52:02 +0300

    ..

--- main.py
+++ main.py
@@ -11,19 +11,21 @@ except ImportError:
 
 
 def parse_mp8_report(xml_file):
-    ns = "http://www.ptsecurity.ru/reports" #namespace
+    ns = "http://www.ptsecurity.ru/reports" #namespace = xmlns
 
     root = ET.parse(xml_file).getroot()
 
     for vln in root.findall("./{%s}vulners/{%s}vulner" %(ns, ns)):
         #print (vln.tag)
         print (vln.get("id"))
-
+        print ( "".join(vln.find("./{%s}title" %(ns)).itertext()))# get text between tags
+        for vlnid in vln.findall("./{%s}global_id" %(ns)):
+            print (vlnid.get("name")+"  "+vlnid.get("value"))
 
 
 
 parser = argparse.ArgumentParser(description='MP8 XML')
-parser.add_argument('-i', dest="xml_file", help="Путь к файлу отчета MP8", metavar='in-file', required=True )
+parser.add_argument('-i', dest="xml_file", help="Путь к файлу отчета MP8", required=True )
 
 try:
     results = parser.parse_args()
